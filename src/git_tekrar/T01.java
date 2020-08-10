9f3d3f938b345b7c814311092f51c0a2e0bccca2
marufozkan <69048132+marufozkan@users.noreply.github.com>
2020-08-10 19:05:37 +0300

    Update T01.java
    github ta degisiklik yaptik

--- src/git_tekrar/T01.java
+++ src/git_tekrar/T01.java
@@ -3,7 +3,9 @@ package git_tekrar;
 public class T01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		
+		System.out.println("hayat beni neden yoruyorsun");
 
 	}
 
